ba3aef508943369d8af595c27d99f686d4fb031e
JordonGarza <jordon.k.garza@gmail.com>
2019-04-11 17:16:14 -0500

    Update admin.py

--- DatabasesWebsite/users/admin.py
+++ DatabasesWebsite/users/admin.py
@@ -7,6 +7,12 @@ from django.contrib.auth.forms import UserCreationForm
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
 from .models import Book
+from .models import Bookcopy
+from .models import Item
+from .models import Movie
+from .models import Moviecopy
+from .models import Sheetmusic
+from .models import Musiccopy
 
 
 class UserCreateForm(UserCreationForm):
@@ -28,5 +34,15 @@ class CustomUserAdmin(UserAdmin):
         }),
     )
 
+  
+
+
 admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Book)
+admin.site.register(Bookcopy)
+admin.site.register(Item)
+admin.site.register(Movie)
+admin.site.register(Moviecopy)
+admin.site.register(Sheetmusic)
+admin.site.register(Musiccopy)
+
