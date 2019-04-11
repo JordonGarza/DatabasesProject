aaa6dbded85461bdf1c362fb1aeff8453f1352e5
Brianm0823 <49293194+Brianm0823@users.noreply.github.com>
2019-04-11 00:31:57 -0500

    Added catalog app

--- DatabasesWebsite/library/settings.py
+++ DatabasesWebsite/library/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
+    'catalog.apps.CatalogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users',
 ]
 
 MIDDLEWARE = [
