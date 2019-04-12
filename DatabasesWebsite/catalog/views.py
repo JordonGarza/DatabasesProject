50b1e8e68ad560c0e6038938c53c08063d14ad22
Brianm0823 <49293194+Brianm0823@users.noreply.github.com>
2019-04-12 00:26:01 -0500

    Worked on catalog app
    Still have to add search bar and other folter options

--- DatabasesWebsite/catalog/views.py
+++ DatabasesWebsite/catalog/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from users.models import Book
+from users.models import Movie
+from users.models import Sheetmusic
+
 
 
 def index(request):
-	return HttpResponse("TEST")
+	search_for_filter = 'Books'
+	context = {}
+	if request.GET.get('search_for'):
+		search_for_filter = request.GET.get('search_for')
+
+	if search_for_filter == 'Books':
+		context = {
+		'books' : Book.objects.all()
+		}	
+	elif search_for_filter == 'Movies':
+		context = {
+		'movies' : Movie.objects.all()
+		}
+	elif search_for_filter == 'Music':
+		context = {
+		'musics' : Sheetmusic.objects.all()
+		}
+
+
+	return render(request, 'catalog/home.html', context)
