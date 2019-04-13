0b45b3aadc96ce41ec778451c48bcc7bb359dca4
Tu Van <tuvannguyen33@gmail.com>
2019-04-13 14:15:40 -0500

    Adding patron page

--- DatabasesWebsite/users/urls.py
+++ DatabasesWebsite/users/urls.py
@@ -1,7 +1,10 @@
 # users/urls.py
+#Reference for adding urlpatterns:
+# https://docs.djangoproject.com/en/2.2/topics/http/urls/
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
+    path('patron/', views.loginPatron, name='patron'),
 ]
--- DatabasesWebsite/users/views.py
+++ DatabasesWebsite/users/views.py
@@ -1,6 +1,11 @@
 # users/views.py
+# reference for writing views: 
+#https://docs.djangoproject.com/en/2.2/topics/http/views/
 from django.urls import reverse_lazy
 from django.views import generic
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 from .forms import CustomUserCreationForm
 
@@ -8,3 +13,10 @@ class SignUp(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+#See link for use of login_required decorator
+# https://docs.djangoproject.com/en/2.2/topics/auth/default/    
+@login_required(login_url="../login/")
+def loginPatron(request):
+    #function redirects user to webpage for patrons (non-admin) users
+    return render(request, 'patron.html')
