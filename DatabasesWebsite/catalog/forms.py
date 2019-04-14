9c38bd4cba7ff38b8359d41bd9207dca013a9496
Brianm0823 <49293194+Brianm0823@users.noreply.github.com>
2019-04-13 22:41:03 -0500

    Updated catalog app

--- DatabasesWebsite/catalog/forms.py
+++ DatabasesWebsite/catalog/forms.py
@@ -1,1 +1,5 @@
 from django import forms
+
+class CatalogForm(forms.Form):
+	search_term = forms.CharField(label='Search', max_length=100, required=False)
+	filter_choice = forms.ChoiceField(label='', choices=[("All", "All")] + [("Books", "Books")] + [("Movies", "Movies")] + [("Music", "Music")])
--- DatabasesWebsite/catalog/views.py
+++ DatabasesWebsite/catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from . forms import CatalogForm
 from users.models import Book
 from users.models import Movie
 from users.models import Sheetmusic
@@ -6,22 +7,61 @@ from users.models import Sheetmusic
 
 
 def index(request):
-	search_for_filter = 'Books'
+	search_for_filter = 'All'
+	search_term = ''
 	context = {}
-	if request.GET.get('search_for'):
-		search_for_filter = request.GET.get('search_for')
 
-	if search_for_filter == 'Books':
+
+	form = CatalogForm(request.GET)
+	
+	if form.is_valid():
+		search_term = form.cleaned_data['search_term']
+		search_for_filter = form.cleaned_data['filter_choice']	
+	form = CatalogForm()
+
+	if search_for_filter == 'Books' and search_term == '':
 		context = {
+		'form' : form,
 		'books' : Book.objects.all()
 		}	
-	elif search_for_filter == 'Movies':
+	elif search_for_filter == 'Books' and search_term != '':
 		context = {
+		'form' : form,
+		'books' : Book.objects.filter(title__icontains=search_term)
+		}
+	elif search_for_filter == 'Movies' and search_term == '':
+		context = {
+		'form' : form,
 		'movies' : Movie.objects.all()
 		}
-	elif search_for_filter == 'Music':
+	elif search_for_filter == 'Movies' and search_term != '':
 		context = {
-		'musics' : Sheetmusic.objects.all()
+		'form' : form,
+		'books' : Movie.objects.filter(title__icontains=search_term)
+		}
+	elif search_for_filter == 'Music' and search_term == '':
+		context = {
+		'form' : form,
+		'music' : Sheetmusic.objects.all()
+		}
+	elif search_for_filter == 'Music' and search_term != '':
+		context = {
+		'form' : form,
+		'music' : Sheetmusic.objects.filter(title__icontains=search_term)
+		}
+	elif search_for_filter == 'All' and search_term != '':
+		context = {
+		'form' : form,
+		'books' : Book.objects.filter(title__icontains=search_term),
+		'movies' : Movie.objects.filter(title__icontains=search_term),
+		'music' : Sheetmusic.objects.filter(title__icontains=search_term)
+		}
+	else:
+		context = {
+		'form' : form,
+		'books' : Book.objects.all(),
+		'movies' : Movie.objects.all(),
+		'music' : Sheetmusic.objects.all()
 		}
 
 
