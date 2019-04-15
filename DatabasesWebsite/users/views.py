8eed87e46990a575cc3092dbce9eb1aacff7dac2
JordonGarza <jordon.k.garza@gmail.com>
2019-04-15 16:28:17 -0500

    Update views.py

--- DatabasesWebsite/users/views.py
+++ DatabasesWebsite/users/views.py
@@ -1,24 +1,25 @@
 # users/views.py
-# reference for writing views: 
-#https://docs.djangoproject.com/en/2.2/topics/http/views/
 from django.urls import reverse_lazy
 from django.views import generic
-from django.http import HttpResponse
-from django.shortcuts import render, redirect
 from django import forms
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
 from .forms import CustomUserCreationForm
 from .forms import SignOut
 from .forms import SignIn
+from .forms import pay
 from .models import CustomUser
+from .models import UsersCustomuser
+from .models import Finetransactions
 from django.views.generic.edit import FormView
+import datetime
 
 class SignUp(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
-    
+
 class checkOut(generic.CreateView):
     form_class = SignOut
     template_name = 'checkOut.html'
@@ -49,3 +50,23 @@ def add_checkIn(request):
             form = SignIn()
  
             return render(request, "checkIn.html", {'form': form})
+
+def pay_Fine(request):
+        if request.method == "POST":
+            form = pay(request.POST)
+            if form.is_valid():
+                model_instance = form.save(commit=False)
+                TheAmount = form.instance.amount
+                user_id = request.user.id
+                user = UsersCustomuser.objects.get(id = user_id)
+                TransDate = datetime.date.today()
+                TheType = 'PAYMENT'
+                model_instance = Finetransactions(userid = user, transtype = TheType, transactiondate =  TransDate, amount = TheAmount)
+                model_instance.save()
+                return redirect('/')
+        else:
+ 
+            form = pay()
+ 
+            return render(request, "payFine.html", {'form': form})
+
