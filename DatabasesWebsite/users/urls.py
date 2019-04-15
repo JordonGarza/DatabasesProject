d86c61d4a0cfed858e015f51469a4cc4896089b3
JordonGarza <jordon.k.garza@gmail.com>
2019-04-15 16:29:35 -0500

    Update urls.py

--- DatabasesWebsite/users/urls.py
+++ DatabasesWebsite/users/urls.py
@@ -1,11 +1,13 @@
 # users/urls.py
-#Reference for adding urlpatterns:
-# https://docs.djangoproject.com/en/2.2/topics/http/urls/
 from django.urls import path
 from . import views
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
     path('checkOut/', views.add_model, name='add_model'),
     path('checkIn/', views.add_checkIn, name='add_checkIn'),
+    path('PayFine/', views.pay_Fine, name='pay_Fine'),
 ]
